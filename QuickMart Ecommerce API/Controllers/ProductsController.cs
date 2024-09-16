a3253489c7dc784acd0b23f1c58d20690a03c3a0
Mohammed Rammah <mohammedrammah83@gmail.com>
2024-09-16 03:43:54 +0300

    Add project files.

---
+++ QuickMart Ecommerce API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuickMart_Ecommerce_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "This is will be a list of Products!";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "This is will be a spacified Products!";
+        }
+    }
+
+}
