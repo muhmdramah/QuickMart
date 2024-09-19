94fbd19c60292845c32188bcc510d6290e835aae
Mohammed Rammah <mohammedrammah83@gmail.com>
2024-09-19 04:38:03 +0300

    feat: Added ISpecification and BaseSpecification for flexible Product entity queries with includes

---
+++ Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+        public BaseSpecification(Expression<Func<T, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+
+        public Expression<Func<T, bool>> Criteria { get; }
+
+        public List<Expression<Func<T, object>>> Includes { get; }
+            = new List<Expression<Func<T, object>>>();
+
+        protected void AddIncludes(Expression<Func<T, object>> include)
+        {
+            Includes.Add(include);
+        }
+    }
+}
---
+++ Core/Specifications/ISpecification.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+    public interface ISpecification<T>
+    {
+        Expression<Func<T, bool>> Criteria { get; }
+        List<Expression<Func<T, object>>> Includes { get; }
+    }
+}
