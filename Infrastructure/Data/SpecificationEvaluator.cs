1bde6378783c0354f8737d0f6dbea9f9aa7c8762
Mohammed Rammah <mohammedrammah83@gmail.com>
2024-09-19 04:48:19 +0300

    feat: Added SpecificationEvaluator to apply criteria and includes for queries

---
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,21 @@
+﻿using Core.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    internal class SpecificationEvaluator<T> where T : class
+    {
+        public static IQueryable<T> GetQuery(IQueryable<T> inputQuery, ISpecification<T> specification)
+        {
+            var query = inputQuery;
+
+            if (specification.Criteria != null)
+                query = inputQuery.Where(specification.Criteria);
+
+            query = specification.Includes
+                .Aggregate(query, (current, include) => current.Include(include));
+
+            return query;
+        }
+    }
+}
