d647dac88b28ca19d49dc60a718db70386d146d3
Mohammed Rammah <mohammedrammah83@gmail.com>
2024-09-29 06:25:55 +0300

    feat: Add Address and ApplicationUser classes for user profile management

---
+++ Core/Identity/Address.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Identity
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
---
+++ Core/Identity/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Core.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string DisplayName { get; set; }
+        public Address Address { get; set; }
+    }
+}
