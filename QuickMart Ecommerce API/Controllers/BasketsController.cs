d70b1f3aeac91e195e179f436b544d1ec1d75766
Mohammed Rammah <mohammedrammah83@gmail.com>
2024-09-29 04:07:15 +0300

    feat(basket): add BasketsController with CRUD operations
    - Implemented GetBasketById to retrieve customer basket by ID
    - Added UpdateBasket for updating the customer basket
    - Included DeleteBasket to remove a customer basket
    - Integrated with IBasketRepository for basket management via Redis

---
+++ QuickMart Ecommerce API/Controllers/BasketsController.cs
@@ -0,0 +1,39 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuickMart_Ecommerce_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public BasketsController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
+        {
+            var basket = await _basketRepository.GetBasketAsync(id);
+
+            return Ok(basket ?? new CustomerBasket(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        {
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+            return Ok(updatedBasket);
+        }
+
+        [HttpDelete]
+        public async Task DeleteBasket(string id)
+        {
+            await _basketRepository.DeleteBasketAsync(id);
+        }
+    }
+}
