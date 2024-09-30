8ef021746402eb34f02b77b7edadc850fe17b86f
Mohammed Rammah <mohammedrammah83@gmail.com>
2024-09-30 03:50:39 +0300

    feat: Implemented JWT token generation with ITokenService and TokenService
    - Added ITokenService interface to define token creation for ApplicationUser.
    - Implemented TokenService for generating JWT tokens using HMAC SHA-512 encryption.
    - Configured claims for user email and display name.
    - Token expiration set to 7 days, with issuer retrieved from configuration.

---
+++ Core/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using Core.Identity;
+
+namespace Core.Interfaces
+{
+    public interface ITokenService
+    {
+        public string CreateToken(ApplicationUser user);
+    }
+}
---
+++ Infrastructure/Services/TokenService.cs
@@ -0,0 +1,48 @@
+﻿using Core.Identity;
+using Core.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Infrastructure.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly SymmetricSecurityKey _symmetricSecurityKey;
+
+        public TokenService(IConfiguration configuration, SymmetricSecurityKey symmetricSecurityKey)
+        {
+            _configuration = configuration;
+            _symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Token:jwt"]));
+        }
+
+        public string CreateToken(ApplicationUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.DisplayName),
+            };
+
+            var credentials = new SigningCredentials(_symmetricSecurityKey,
+                SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = credentials,
+                Issuer = _configuration["Token:Issuer"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
