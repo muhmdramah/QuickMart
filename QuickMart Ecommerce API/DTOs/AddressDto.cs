81b05efce2d89727e582ff7402bf2449d8bf0ab6
Mohammed Rammah <mohammedrammah83@gmail.com>
2024-10-01 02:58:16 +0300

    feat: implement enhanced data validation with required attributes for RegisterDto and AddressDto

--- QuickMart Ecommerce API/DTOs/AddressDto.cs
+++ QuickMart Ecommerce API/DTOs/AddressDto.cs
@@ -1,12 +1,19 @@
-﻿namespace QuickMart_Ecommerce_API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuickMart_Ecommerce_API.DTOs
 {
     public class AddressDto
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
         public string Street { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
         public string ZipCode { get; set; }
     }
 }
--- QuickMart Ecommerce API/DTOs/IdentityUser/RegisterDto.cs
+++ QuickMart Ecommerce API/DTOs/IdentityUser/RegisterDto.cs
@@ -1,9 +1,15 @@
-﻿namespace QuickMart_Ecommerce_API.DTOs.IdentityUser
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuickMart_Ecommerce_API.DTOs.IdentityUser
 {
     public class RegisterDto
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
