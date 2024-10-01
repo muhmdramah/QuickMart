5d65b4ffa31e1c318fdd51fcc3e2d1d69707f602
Mohammed Rammah <mohammedrammah83@gmail.com>
2024-10-01 03:53:46 +0300

    feat: add entity configurations for DeliveryMethod, Order, and OrderItem
    - Implemented DeliveryMethodConfiguration to define the precision for the Price property
    - Added OrderConfiguration to manage owned entity ShipToAddress, ensure required navigation, configure OrderStatus conversion, and define relationships with OrderItems
    - Implemented OrderItemConfiguration to configure owned entity ItemOrdered and set Price precision

--- Core/Entities/OrderAggregate/OrderItem.cs
+++ Core/Entities/OrderAggregate/OrderItem.cs
@@ -3,14 +3,14 @@
     public class OrderItem : BaseEntity
     {
         public OrderItem() { }
-        public OrderItem(ProductItemOrder productItem, decimal price, int quantity)
+        public OrderItem(ProductItemOrder itemOrderd, decimal price, int quantity)
         {
-            ProductItem = productItem;
+            ItemOrderd = itemOrderd;
             Price = price;
             Quantity = quantity;
         }
 
-        public ProductItemOrder ProductItem { get; set; }
+        public ProductItemOrder ItemOrderd { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
     }
---
+++ Infrastructure/Data/Config/OrderConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.OwnsOne(o => o.ShipToAddress, a =>
+            {
+                a.WithOwner();
+            });
+
+            builder.Navigation(a => a.ShipToAddress).IsRequired();
+
+            // Prsing Status into OrderStatus
+            builder.Property(a => a.Status)
+                .HasConversion(
+                    o => o.ToString(),
+                    o => (OrderStatus)Enum.Parse(typeof(OrderStatus), o));
+
+            builder.HasMany(o => o.OrderItems).WithOne().OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
